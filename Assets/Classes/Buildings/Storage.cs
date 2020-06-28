5f68c1d92721ac34e960c17ca49f665f13f6e5c2
lorexsan <lorenzosani97@gmail.com>
2020-06-28 09:15:31 +0100

    Enable storage functionality

--- Assets/Classes/Buildings/Storage.cs
+++ Assets/Classes/Buildings/Storage.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 public class Storage : Building
 {
+  int storage;
   public Storage(){
     prefab = (GameObject)Resources.Load("Prefabs/Storage", typeof(GameObject));
     level = 1;
@@ -13,5 +14,10 @@ public class Storage : Building
     value = 30;
     cost = new int[3] { 100, 150, 0 };
     completionTime = DateTime.UtcNow.AddSeconds(value * level);
+    storage = 1000*level;
+  }
+
+  public override void startFunctionality(ControllerScript controller){
+    controller.getUser().setStorage(storage);
   }
 }
--- Assets/Classes/User.cs
+++ Assets/Classes/User.cs
@@ -9,6 +9,7 @@ public class User
   int bounty;
   int[] resources;
   int pearl;
+  int storage;
 
   public User(int _id, int _serverId, Village _village)
   {
@@ -16,8 +17,9 @@ public class User
     village = _village;
     serverId = _serverId;
     bounty = 0;
-    resources = new int[3] { 900, 900, 99 };
+    resources = new int[3] { 500, 500, 100 };
     pearl = 5;
+    storage = 500;
   }
 
   public int getId()
@@ -42,12 +44,14 @@ public class User
 
   public void setResources(int[] r)
   {
-    resources = r;
+    for(int i=0; i<r.Length; i++) {
+      resources[i] = (r[i] > storage) ? storage : r[i];
+    }
   }
 
   public void increaseResource(int i, int n)
   {
-    resources[i] = resources[i] + n;
+    resources[i] = (resources[i]+n > storage) ? storage : resources[i] + n;
   }
 
   public int getPearl()
@@ -63,5 +67,9 @@ public class User
   public void addBounty(int value)
   {
     bounty += value;
+  }
+
+  public void setStorage(int s){
+    storage = s;
   }
 }
